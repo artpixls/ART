cc3b00332e706c4276c51f2d4865b11649db739d
Alberto Griggio <alberto.griggio@gmail.com>
2024-11-23 16:34:34 +0100

    osx bundle_ART: fixed setting of DYLD_LIBRARY_PATH

--- tools/osx/bundle_ART.py
+++ tools/osx/bundle_ART.py
@@ -249,19 +249,15 @@ export ART_restore_GSETTINGS_SCHEMA_DIR=$GSETTINGS_SCHEMA_DIR
 export ART_restore_XDG_DATA_DIRS=$XDG_DATA_DIRS
 d=$(dirname "$0")/..
 t=$(/usr/bin/mktemp -d)
+export DYLD_LIBRARY_PATH="$d/Frameworks"
 export GTK_CSD=0
-#export GDK_PIXBUF_MODULE_FILE="$d/Resources/etc/gtk-3.0/gdk-pixbuf.loaders"
-"$d/Resources/gdk-pixbuf-query-loaders" "$d/Frameworks/"libpixbufloader-*.so > "$t/loader.cache"
+"$d/Resources/gdk-pixbuf-query-loaders" "$d/Frameworks/"libpixbufloader-svg.so > "$t/loader.cache"
 "$d/Resources/gtk-query-immodules-3.0" "$d"/Frameworks/im-*.so > "$t/gtk.immodules"
 export GDK_PIXBUF_MODULE_FILE="$t/loader.cache"
 export GTK_IM_MODULE_FILE="$t/gtk.immodules"
 export GDK_PIXBUF_MODULEDIR="$d/Frameworks"
-#export GIO_MODULE_DIR="$d/Frameworks/gio/modules"
-export DYLD_LIBRARY_PATH="$d/Frameworks"
 export FONTCONFIG_FILE="$d/Resources/fonts.conf"
-#export ART_EXIFTOOL_BASE_DIR="$d/Resources/exiftool"
 export GTK_PATH="$d/Resources/etc/gtk-3.0"
-#export GTK_IM_MODULE_FILE="$d/Resources/etc/gtk-3.0/gtk.immodules"
 export GSETTINGS_SCHEMA_DIR="$d/Resources/share/glib-2.0/schemas"
 export XDG_DATA_DIRS="$d/Resources/share"
 export GDK_RENDERING=similar
@@ -274,9 +270,7 @@ export GTK_OVERLAY_SCROLLING=0
 export ART_restore_GIO_MODULE_DIR=$GIO_MODULE_DIR
 export ART_restore_DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
 d=$(dirname "$0")/..
-#export GIO_MODULE_DIR="$d/Frameworks/gio/modules"
 export DYLD_LIBRARY_PATH="$d/Frameworks"
-#export ART_EXIFTOOL_BASE_DIR="$d/lib/exiftool"
 exec "$d/MacOS/.ART-cli.bin" "$@"
 """)
     for name in ('ART', 'ART-cli'):
